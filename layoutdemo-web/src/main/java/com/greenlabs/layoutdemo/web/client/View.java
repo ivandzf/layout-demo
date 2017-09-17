2dc866bdae7407f54d3b6defec138976a100817d
Ivan-TI <divanandazf@gmail.com>
2017-09-17 21:56:00 +0700

    fix layout

--- layoutdemo-web/src/main/java/com/greenlabs/layoutdemo/web/client/View.java
+++ layoutdemo-web/src/main/java/com/greenlabs/layoutdemo/web/client/View.java
@@ -1,8 +1,9 @@
 package com.greenlabs.layoutdemo.web.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.sencha.gxt.widget.core.client.button.TextButton;
+
 
 /**
  * Created by Ivan-TI on 9/15/2017
@@ -11,7 +12,8 @@ public class View implements EntryPoint {
 
     @Override
     public void onModuleLoad() {
-
-        RootPanel.get().add(new Label("Hello World !!"));
+        TextButton btn = new TextButton("Hello World!!");
+        RootPanel.get().add(btn);
+        System.out.println("ini kebuka");
     }
 }
